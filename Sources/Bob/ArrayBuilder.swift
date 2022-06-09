3a2b95d27d846950d4363fa6aa0a4499c00e7c6e
David Roman <2538074+davdroman@users.noreply.github.com>
2022-06-09 22:34:48 +0100

    Use `buildPartialBlock` on Swift 5.7

--- Sources/Bob/ArrayBuilder.swift
+++ Sources/Bob/ArrayBuilder.swift
@@ -2,14 +2,47 @@
 public struct ArrayBuilder<Element> {
     public typealias Array = Swift.Array<Element>
 
-    public static func buildArray(_ arrays: [Array]) -> Array { arrays.reduce([], +) }
-    public static func buildBlock(_ arrays: Array...) -> Array { arrays.reduce([], +) }
-    public static func buildEither(first array: Array) -> Array { array }
-    public static func buildEither(second array: Array) -> Array { array }
-    public static func buildExpression(_ element: Element) -> Array { [element] }
-    public static func buildExpression(_ array: Array) -> Array { array }
-    public static func buildLimitedAvailability(_ array: Array) -> Array { array }
-    public static func buildOptional(_ array: Array?) -> Array { array ?? [] }
+    #if compiler(>=5.7)
+    public static func buildPartialBlock(first: Array) -> Array {
+        first
+    }
+
+    public static func buildPartialBlock(accumulated: Array, next: Array) -> Array {
+        accumulated + next
+    }
+    #else
+    public static func buildBlock(_ arrays: Array...) -> Array {
+        arrays.reduce([], +)
+    }
+    #endif
+
+    public static func buildArray(_ arrays: [Array]) -> Array {
+        arrays.reduce([], +)
+    }
+
+    public static func buildEither(first array: Array) -> Array {
+        array
+    }
+
+    public static func buildEither(second array: Array) -> Array {
+        array
+    }
+
+    public static func buildExpression(_ element: Element) -> Array {
+        [element]
+    }
+
+    public static func buildExpression(_ array: Array) -> Array {
+        array
+    }
+
+    public static func buildLimitedAvailability(_ array: Array) -> Array {
+        array
+    }
+
+    public static func buildOptional(_ array: Array?) -> Array {
+        array ?? []
+    }
 }
 
 extension Array {
