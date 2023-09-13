77826e1b846148252163018b092c13030896cc32
David Roman <2538074+davdroman@users.noreply.github.com>
2023-09-13 12:56:40 +0100

    wip

--- Sources/Builders/_AppendableCollectionBuilder.swift
+++ Sources/Builders/_AppendableCollectionBuilder.swift
@@ -2,7 +2,6 @@
 public struct _AppendableCollectionBuilder<Collection: _AppendableCollection> {
     public typealias Element = Collection.Element
 
-    #if swift(>=5.7)
     @inlinable
     public static func buildPartialBlock(first: Collection) -> Collection {
         first
@@ -13,43 +12,6 @@ public struct _AppendableCollectionBuilder<Collection: _AppendableCollection> {
         var accumulated = accumulated
         accumulated.append(contentsOf: next)
         return accumulated
-    }
-    #else
-    public static func buildBlock(_ components: Collection...) -> Collection {
-        components.reduce(into: Collection()) { $0.append(contentsOf: $1) }
-    }
-    #endif
-
-    public static func buildArray(_ components: [Collection]) -> Collection {
-        components.reduce(into: Collection()) { $0.append(contentsOf: $1) }
-    }
-
-    @inlinable
-    public static func buildEither(first component: Collection) -> Collection {
-        component
-    }
-
-    @inlinable
-    public static func buildEither(second component: Collection) -> Collection {
-        component
-    }
-
-    public static func buildExpression(_ element: Element) -> Collection {
-        Collection(element)
-    }
-
-    @inlinable
-    public static func buildExpression(_ component: Collection) -> Collection {
-        component
-    }
-
-    @inlinable
-    public static func buildLimitedAvailability(_ component: Collection) -> Collection {
-        component
-    }
-
-    public static func buildOptional(_ component: Collection?) -> Collection {
-        component ?? Collection()
     }
 }
 
