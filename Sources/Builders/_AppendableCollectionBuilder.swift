6e5d54297d8bb91a81e36e8ec41bc479f99c2606
David Roman <2538074+davdroman@users.noreply.github.com>
2023-09-13 14:45:29 +0100

    wip

--- Sources/Builders/_AppendableCollectionBuilder.swift
+++ Sources/Builders/_AppendableCollectionBuilder.swift
@@ -13,6 +13,38 @@ public struct _AppendableCollectionBuilder<Collection: _AppendableCollection> {
         accumulated.append(contentsOf: next)
         return accumulated
     }
+
+    public static func buildArray(_ components: [Collection]) -> Collection {
+        components.reduce(into: Collection()) { $0.append(contentsOf: $1) }
+    }
+
+    @inlinable
+    public static func buildEither(first component: Collection) -> Collection {
+        component
+    }
+
+    @inlinable
+    public static func buildEither(second component: Collection) -> Collection {
+        component
+    }
+
+    public static func buildExpression(_ element: Element) -> Collection {
+        Collection(element)
+    }
+
+    @inlinable
+    public static func buildExpression(_ component: Collection) -> Collection {
+        component
+    }
+
+    @inlinable
+    public static func buildLimitedAvailability(_ component: Collection) -> Collection {
+        component
+    }
+
+    public static func buildOptional(_ component: Collection?) -> Collection {
+        component ?? Collection()
+    }
 }
 
 extension _AppendableCollection {
