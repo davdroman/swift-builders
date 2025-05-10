4222bb8125b751185740cd9982dc961ea1743cc9
David Roman <2538074+davdroman@users.noreply.github.com>
2025-05-10 19:04:24 +0100

    WIP

--- Sources/Builders/DictionaryBuilder.swift
+++ Sources/Builders/DictionaryBuilder.swift
@@ -1,52 +1,52 @@
 @resultBuilder
 public struct DictionaryBuilder<Key: Hashable, Value> {
 	@inlinable
-	public static func buildBlock() -> Dictionary<Key, Value> {
+	public static func buildBlock() -> [Key: Value] {
 		[:]
 	}
 
 	@inlinable
-	public static func buildPartialBlock(first: Dictionary<Key, Value>) -> Dictionary<Key, Value> {
+	public static func buildPartialBlock(first: [Key: Value]) -> [Key: Value] {
 		first
 	}
 
 	@inlinable
-	public static func buildPartialBlock(accumulated: Dictionary<Key, Value>, next: Dictionary<Key, Value>) -> Dictionary<Key, Value> {
+	public static func buildPartialBlock(accumulated: [Key: Value], next: [Key: Value]) -> [Key: Value] {
 		accumulated.merging(next, uniquingKeysWith: { $1 })
 	}
 
 	@inlinable
-	public static func buildArray(_ components: [Dictionary<Key, Value>]) -> Dictionary<Key, Value> {
+	public static func buildArray(_ components: [[Key: Value]]) -> [Key: Value] {
 		Dictionary(components.joined().map { ($0, $1) }, uniquingKeysWith: { $1 })
 	}
 
 	@inlinable
-	public static func buildEither(first component: Dictionary<Key, Value>) -> Dictionary<Key, Value> {
+	public static func buildEither(first component: [Key: Value]) -> [Key: Value] {
 		component
 	}
 
 	@inlinable
-	public static func buildEither(second component: Dictionary<Key, Value>) -> Dictionary<Key, Value> {
+	public static func buildEither(second component: [Key: Value]) -> [Key: Value] {
 		component
 	}
 
 	@inlinable
-	public static func buildExpression(_ element: (Key, Value)) -> Dictionary<Key, Value> {
+	public static func buildExpression(_ element: (Key, Value)) -> [Key: Value] {
 		[element.0: element.1]
 	}
 
 	@inlinable
-	public static func buildExpression(_ component: Dictionary<Key, Value>) -> Dictionary<Key, Value> {
+	public static func buildExpression(_ component: [Key: Value]) -> [Key: Value] {
 		component
 	}
 
 	@inlinable
-	public static func buildLimitedAvailability(_ component: Dictionary<Key, Value>) -> Dictionary<Key, Value> {
+	public static func buildLimitedAvailability(_ component: [Key: Value]) -> [Key: Value] {
 		component
 	}
 
 	@inlinable
-	public static func buildOptional(_ component: Dictionary<Key, Value>?) -> Dictionary<Key, Value> {
+	public static func buildOptional(_ component: [Key: Value]?) -> [Key: Value] {
 		component ?? [:]
 	}
 }
